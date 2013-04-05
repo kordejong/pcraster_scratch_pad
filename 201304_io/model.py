e6e2b95405aaa8d4a1aef7bacfa3985f7f6f96a5
Kor de Jong <k.dejong1@uu.nl>
2013-04-05 17:13:13 +0200

    Wip

--- 201304_io/model.py
+++ 201304_io/model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import numpy
 import psutil
 import pcraster
 import pcraster.framework
@@ -10,8 +11,8 @@ import pcraster.framework
 # During post-processing, reading these values back in takes ages.
 
 first_time_step = 1
-last_time_step = 100 # 10000
-nr_samples = 50 # 500
+last_time_step = 5000 # 10000
+nr_samples = 250 # 500
 nr_state_variables = 10
 nr_locations_to_write = 10
 
@@ -19,6 +20,13 @@ nr_locations_to_write = 10
 class Model(
     pcraster.framework.DynamicModel,
     pcraster.framework.MonteCarloModel):
+
+    @staticmethod
+    def generate_name(
+            variable_id,
+            sample_nr,
+            timestep):
+        return "{}/state{}_{}".format(sample_nr, variable_id, timestep)
 
     def __init__(self,
             clone_map,
@@ -30,16 +38,21 @@ class Model(
         self.nr_state_variables = nr_state_variables
         self.nr_locations_to_write = nr_locations_to_write
         self.process = psutil.Process(os.getpid())
-        self.log_file = file("memory_report.col", "w")
+        self.memory_log_file = file("memory.col", "w")
+        self.runtime_log_file = file("runtime.col", "w")
 
-    def print_memory_used(self):
-        nr_mbytes = self.process.get_memory_info()[0] / 1048576.0
-        self.log_file.write("{}\n".format(nr_mbytes))
+    def log_memory_used(self):
+        nr_mbytes = self.process.get_memory_info().rss / 1048576.0
+        self.memory_log_file.write("{}\n".format(nr_mbytes))
 
-    def generate_name(self,
-            variable_id):
-        return "{}/state{}_{}".format(self.currentSampleNumber(), variable_id,
-            self.currentTimeStep())
+    def log_time_spent(self):
+        # System + user.
+        cpu_time = sum(self.process.get_cpu_times())
+        self.runtime_log_file.write("{}\n".format(cpu_time))
+
+    def log_process_statistics(self):
+        self.log_memory_used()
+        self.log_time_spent()
 
     def premcloop(self):
         pass
@@ -50,14 +63,24 @@ class Model(
     def dynamic(self):
         # Per state variable a file with nr_locations_to_write values.
         for i in xrange(1, self.nr_state_variables + 1):
-            filename = self.generate_name(i)
-            file(filename, "w").write(", ".join([str(i) for i in xrange(
+            filename = Model.generate_name(i, self.currentSampleNumber(),
+                self.currentTimeStep())
+            file(filename, "w").write("\t".join([str(i) for i in xrange(
                 self.nr_locations_to_write)]))
-
-        self.print_memory_used()
+            self.log_process_statistics()
 
     def postmcloop(self):
-        pass
+        for i in xrange(1, self.nr_state_variables + 1):
+            output = []
+            for timestep in xrange(1, self.nrTimeSteps() + 1):
+                all_samples = []
+                for sample_nr in xrange(1, self.nrSamples() + 1):
+                    filename = Model.generate_name(i, sample_nr, timestep)
+                    array = numpy.loadtxt(filename)
+                    all_samples.append(array)
+                    self.log_process_statistics()
+                output.append(all_samples)
+            output_as_array = numpy.array(output)
 
 
 model = Model("clone.map", nr_state_variables, nr_locations_to_write)
