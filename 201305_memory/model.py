e78db48b36823f79aa9d041b68877d6f62d0d704
Kor de Jong <k.dejong@geo.uu.nl>
2013-05-30 09:52:52 +0200

    Wip

--- 201305_memory/model.py
+++ 201305_memory/model.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import os
+import sys
 import psutil
 import pcraster
 import pcraster.framework
 
 
 first_time_step = 1
-last_time_step = 10
-nr_samples = 3
-nr_state_variables = 64
-nr_spinups = 10
+last_time_step = 100
+nr_samples = 20
+nr_spinups = 50
 log_file = file("memory.col", "w")
 
 
@@ -24,7 +24,6 @@ class Model(
         pcraster.framework.MonteCarloModel.__init__(self)
         pcraster.setclone(clone_map)
         self.log_file = log_file
-        self.nr_state_variables = nr_state_variables
         self.process = psutil.Process(os.getpid())
 
     def print_memory_used(self):
@@ -32,29 +31,17 @@ class Model(
         self.log_file.write("{}\n".format(nr_mbytes))
 
     def premcloop(self):
-        self.dummy = pcraster.scalar(pcraster.readmap("clone.map")) * 5.0
+        # Mmm, this leaks. Member variables leak, local variables don't.
+        self.dummy1 = pcraster.scalar(pcraster.readmap("clone.map")) * 5 * 4 * 3
+
+        # Deleting the instance solves the leak. Why is the instance not
+        # deleted once the instance is overwritten?
+        # del self.dummy1
 
     def initial(self):
         pass
 
     def dynamic(self):
-        # Write state, using unique names.
-        names = [pcraster.framework.generateNameST("dummy{}x".format(i),
-            self.currentSampleNumber(), self.currentTimeStep()) for i in
-                range(1, self.nr_state_variables + 1)]
-
-        # Write state.
-        for name in names:
-            pcraster.report(self.dummy, name)
-
-        # Read state.
-        for name in names:
-            pcraster.readmap(name)
-
-        # Remove state.
-        for name in names:
-            os.remove(name)
-
         self.print_memory_used()
 
     def postmcloop(self):
@@ -63,10 +50,21 @@ class Model(
 
 for spinup_id in xrange(nr_spinups):
     model = Model("clone.map", log_file)
+    print "model (2)", sys.getrefcount(model)
     dynamic_model = pcraster.framework.DynamicFramework(model,
         firstTimestep=first_time_step, lastTimeStep=last_time_step)
-    # dynamic_model.setQuiet(True)
+    print "model (3)", sys.getrefcount(model)
+    print "dynamic_model (2)", sys.getrefcount(dynamic_model)
+    dynamic_model.setQuiet(True)
     monte_carlo_model = pcraster.framework.MonteCarloFramework(dynamic_model,
         nrSamples=nr_samples)
-    # monte_carlo_model.setQuiet(True)
+    print "model (3)", sys.getrefcount(model)
+    print "dynamic_model (3)", sys.getrefcount(dynamic_model)
+    print "monte_carlo_model (2)", sys.getrefcount(monte_carlo_model)
+    monte_carlo_model.setQuiet(True)
     monte_carlo_model.run()
+
+    del monte_carlo_model
+    del dynamic_model
+    print "model (2)", sys.getrefcount(model)
+    print ""
